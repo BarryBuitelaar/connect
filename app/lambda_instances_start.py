46d0a69b748e891301425b3edc8aca3272bca023
Youri Zwetsloot <youri.zwetsloot@inqdo.com>
2020-03-02 10:57:09 +0100

    Resolve left-over merge conflicts

--- app/lambda_instances_start.py
+++ app/lambda_instances_start.py
@@ -1,59 +1,5 @@
-<<<<<<< HEAD
 from . import common
 from app.fn_instances_start_helper import start_instances
 
 def lambda_handler(event, context):
     return common.lambda_handler(file=__file__, event=event, context=context, delegate=start_instances)
-=======
-import logging
-
-import boto3
-from botocore.exceptions import ClientError
-
-from . import common
-
-logger = logging.getLogger(common.logger_name(__file__))
-
-
-def lambda_handler(event, context):
-    '''
-    deals with both starting a single instance, or starting multiple instances and an ASG depending
-    on provided parameters
-
-    required parameters:
-        start single instance:
-            - instance
-            - arn
-            - region
-        ASG:
-            - asgName # if present: resume ASG process
-            - instances
-            - arn
-            - region
-    '''
-    request = common.json_body_as_dict(event)
-
-    arn = request['arn']
-    region = request['region']
-
-    ec2 = common.assume_role('ec2', arn, region)
-    auto_scaling_client = common.assume_role('autoscaling', arn, region)
-
-    if 'instances' in request:
-        instance_ids = request['instances']
-    else:
-        instance_ids = [request['instance']] # if a single instance is provided, wrap in list
-
-    try:
-        ec2.start_instances(InstanceIds=instance_ids)
-    except ClientError as e:
-        return common.throw_error(F'Failed to start instance: {e}')
-
-    if 'asgName' in request:
-        try:
-            auto_scaling_client.resume_processes(AutoScalingGroupName=request['asgName'])
-        except ClientError as e:
-            return common.throw_error(F'Failed to resume {request["asgName"]}: {e}')
-
-    return common.return_response(body={'post_success': F'started instances {instance_ids}'})
->>>>>>> 2925e82f5bad5baa58ead86a9efbb87e5cebbc12
--- app/lambda_instances_stop.py
+++ app/lambda_instances_stop.py
@@ -1,44 +1,6 @@
 from . import common
-<<<<<<< HEAD
 from app.fn_instances_stop_helper import stop_instances
 
 
 def lambda_handler(event, context):
     return common.lambda_handler(file=__file__, event=event, context=context, delegate=stop_instances)
-=======
-
-logger = logging.getLogger(common.logger_name(__file__))
-
-
-def lambda_handler(event, context):
-    '''
-    see lambda_instances_start for interface
-    '''
-    request = common.json_body_as_dict(event)
-
-    arn = request['arn']
-    region = request['region']
-
-    ec2_client = common.assume_role('ec2', arn, region)
-    auto_scaling_client = common.assume_role('autoscaling', arn, region)
-
-    if 'instances' in request:
-        instance_ids = request['instances']
-    else:
-        instance_ids = [request['instance']]
-
-    try:
-        ec2_client.stop_instances(
-            InstanceIds=instance_ids
-        )
-    except ClientError as e:
-        return common.throw_error(F'Failed to stop instances: {e}')
-
-    if 'asgName' in request:
-        try:
-            auto_scaling_client.suspend_processes(AutoScalingGroupName=request['asgName'])
-        except ClientError as e:
-            return common.throw_error(F'Failed to suspend {request["asgName"]}: {e}')
-
-    return common.return_response(body={'post_success': F'stopped instances {instance_ids}'})
->>>>>>> 2925e82f5bad5baa58ead86a9efbb87e5cebbc12
